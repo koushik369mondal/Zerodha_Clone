7c7d14673f3c641c214632041acec963810037f5
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-05 23:42:31 +0530

    Awards section implement

--- frontend/src/landing_page/home/Awards.jsx
+++ frontend/src/landing_page/home/Awards.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
-
+import React from "react";
 export default function Awards() {
     return (
-        <div>
-            <h1>Awards</h1>
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-6 p-5">
+                    <img src="media/images/largestBroker.svg" alt="Award" />
+                </div>
+                <div className="col-6 p-5 mt-5">
+                    <h1>Largest stock broker in India</h1>
+                    <p className="mb-5">
+                        2+ million Zerodha clients contribute to over 15% of all retail
+                        trading volumes in India daily by trading and investing in:{" "}
+                    </p>
+                    <div className="row">
+                        <div className="col-6">
+                            <ul>
+                                <li>
+                                    <p>Future and Options</p>
+                                </li>
+                                <li>
+                                    <p>Commodities and derivatives</p>
+                                </li>
+                                <li>
+                                    <p>Currency derivatives</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-6">
+                            <ul>
+                                <li>
+                                    <p>Stocks & IPOs</p>
+                                </li>
+                                <li>
+                                    <p>Direct Mutual Funds</p>
+                                </li>
+                                <li>
+                                    <p>Bonds and Govt. Securities</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <img src="media/images/press-logos.png" alt="Press Logos" style={{width:"90%"}}/>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
