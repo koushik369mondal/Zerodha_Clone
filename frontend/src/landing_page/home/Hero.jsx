8fe77bc5b43f747a2254a07369bbeade64c4947f
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-05 23:13:56 +0530

    Hero component implementation

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Hero() {
     return (
-        <div>
-            <h1>Hero Section</h1>
+        <div className='container p-5'>
+            <div className='row text-center'>
+                <img src='media/images/homeHero.png' alt='Hero Image' className='mb-5'/>
+                <h1 className='mt-5'>Invest in everything</h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds.</p>
+                <button className='p-2 btn btn-primary fs-5' style={{width:"25%", margin:"0 auto"}}>Signup Now</button>
+            </div>
         </div>
     )
 }
