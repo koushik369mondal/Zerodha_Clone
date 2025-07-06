df4b91da273501c13c78947e1952782bee154cb2
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-06 22:43:26 +0530

    Education component  Implementation

--- frontend/src/landing_page/home/Pricing.jsx
+++ frontend/src/landing_page/home/Pricing.jsx
@@ -5,7 +5,7 @@ export default function Pricing() {
         <div className="container">
             <div className="row">
                 <div className="col-4">
-                    <h1 className="mb-3">Unbeatable pricing</h1>
+                    <h1 className="mb-3 fs-2">Unbeatable pricing</h1>
                     <p>
                         We pioneered the concept of discount broking and price transparency
                         in India. Flat fees and no hidden charges.
@@ -15,7 +15,7 @@ export default function Pricing() {
                     </a>
                 </div>
                 <div className="col-2"></div>
-                <div className="col-6">
+                <div className="col-6 mb-5">
                     <div className="row text-center">
                         <div className="col p-3 border">
                             <h1 className="mb-3">₹0</h1>
