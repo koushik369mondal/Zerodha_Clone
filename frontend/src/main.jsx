2a0f3a1c4ed38d50fb2ff3a6bc0bb9f99404ef86
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-08 23:29:27 +0530

    Hero component implementation for about page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import Signup from "./landing_page/signup/Signup.jsx";
 import AboutPage from "./landing_page/about/AboutPage.jsx";
 import ProductPage from "./landing_page/products/ProductsPage.jsx";
 import PricingPage from "./landing_page/pricing/PricingPage.jsx";
-import SupportPage from "./landing_page/support/SupportPage.jsx";
+import SupportPage from "./landing_page/support/SupportPage.jsx";       
 import Navbar from "./landing_page/Navbar.jsx";
 import Footer from "./landing_page/Footer.jsx";
 import NotFound from "./landing_page/NotFound.jsx";
