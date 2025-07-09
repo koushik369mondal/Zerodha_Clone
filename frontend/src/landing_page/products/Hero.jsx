a3b654155952a58f0b06748f955cb7bfc90e42c8
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-09 23:51:45 +0530

    Enhance Products Page layout with LeftSection component and update Hero styling

--- frontend/src/landing_page/products/Hero.jsx
+++ frontend/src/landing_page/products/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Hero() {
     return (
-        <div className="container text-muted text-center p-5 mt-5 mb-5">
+        <div className="container text-muted text-center p-5 mt-5 mb-5 border-bottom">
             <h1>Zerodha Products</h1>
             <h5 className='mt-4'>Sleek, modern, and intuitive trading platforms</h5>
             <p className='mt-4'>Check out our <a href="">investment offerings <i className="fa-solid fa-arrow-right"></i></a></p>
--- frontend/src/landing_page/products/LeftSection.jsx
+++ frontend/src/landing_page/products/LeftSection.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
 
-export default function LeftSection() {
+export default function LeftSection({
+    imageURL,
+    productName,
+    productDescription,
+    tryDemo,
+    learnMore,
+    googlePlay,
+    appStore,
+}) {
     return (
-        <div>
-            <h1>Left Section</h1>
+        <div className="container text-muted">
+            <div className="row">
+                <div className="col-1"></div>
+                <div className="col-5 p-5">
+                    <img src={imageURL} />
+                </div>
+                <div className="col-1"></div>
+                <div className="col-4 p-5 mt-5">
+                    <h1 className="mb-4">{productName}</h1>
+                    <p>{productDescription}</p>
+                    <div>
+                        <a href={tryDemo} style={{ marginLeft: "30px" }}>Try Demo <i class="fa-solid fa-arrow-right"></i></a>
+                        <a href={learnMore} style={{ marginLeft: "60px" }}>Learn More <i class="fa-solid fa-arrow-right"></i></a>
+                    </div>
+                    <div className="mt-4">
+                        <a href={googlePlay}><img src="media/images/googlePlayBadge.svg" alt="Google Play Badge" /></a>
+                        <a href={appStore} style={{ marginLeft: "30px" }}><img src="media/images/appStoreBadge.svg" alt="App Store Badge" /></a>
+                    </div>
+                    <div className="col-1"></div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
