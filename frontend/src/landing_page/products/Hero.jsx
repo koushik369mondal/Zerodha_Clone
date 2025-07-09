03cb468108e681533d33cca52ba8b749a5e788ee
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-09 23:06:37 +0530

    Hero section implementation for products page

--- frontend/src/landing_page/products/Hero.jsx
+++ frontend/src/landing_page/products/Hero.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Hero() {
     return (
-        <div>
-            <h1>Hero Section</h1>
+        <div className="container text-muted text-center p-5 mt-5 mb-5">
+            <h1>Zerodha Products</h1>
+            <h5 className='mt-4'>Sleek, modern, and intuitive trading platforms</h5>
+            <p className='mt-4'>Check out our <a href="">investment offerings <i className="fa-solid fa-arrow-right"></i></a></p>
         </div>
     )
 }
